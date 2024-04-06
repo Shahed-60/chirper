0468c4c94c4380168f585711f2b0686ed45f88cf
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-06 16:13:55 +0200

    In de web.php heb ik de route toegevoegd om naar de chirps te gaan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+//De resource-methode is een term die wordt gebruikt om een set van objecten te becschrijven die je kunt aanmaken,
+// lezen, bijwerken en verwijderen(dus eigenlijk de crud) ipv dat ik het helemaal zelf moet schrijven.
+// chirps is de naam van de resource en ChirpController is de controller.
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    // de auth en verified middleware zorgen ervoor dat je alleen kan posten als je bent ingelogd en je email is geverifieerd.
+    ->middleware(['auth', 'verified']);
+
+
+
+
+require __DIR__ . '/auth.php';
