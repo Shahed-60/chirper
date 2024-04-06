58405d46d4208ef3b1d91f24ca5d073e9024722e
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-06 23:39:07 +0200

    Ik heb het aangepast zodat niet elke gebruiker een chirper kan aanpassen alleen de eigenaar van de chirps

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Providers;
-
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +20,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('edit-chirp', function ($user, $chirp) {
+            // Return true if the user is authorized to edit the chirp, false otherwise.
+            // For example, you might check if the user is the owner of the chirp.
+            return $user->id === $chirp->user_id;
+        });
     }
 }
